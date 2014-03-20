22173cac914ef3ba9e8b5fb2831af072f63575dd
Jack Cooc <jack.cooc@gmail.com>
2014-03-20 13:25:53 -0400

    initial commit

---
+++ buzzer.js
@@ -0,0 +1,56 @@
+var Buzzes = new Meteor.Collection("buzzes");
+
+if (Meteor.isClient) {
+  Meteor.Router.add({
+    '/buzzer' : 'buzzer',
+    '/buzzes' : 'buzzes'
+  });
+
+  Template.buzzes.allBuzzes = function() {
+    return Buzzes.find();
+  }
+
+  Template.buzzes.events({
+    'click button.reset' : function () {
+      Meteor.call("removeAllBuzzes");
+    }
+  });
+
+  function getPlayerName() {
+    return $("input[name='player-name']").val()
+  }
+
+  Template.buzzer.result = function() {
+    var winner = Buzzes.findOne()
+    if (winner === undefined) return undefined
+
+    var isWinner = Buzzes.findOne().name === getPlayerName()
+    return (result = (isWinner) ? "winner" : "loser")
+  };
+
+  Template.buzzer.events({
+    'click button.buzz' : function () {
+      var name = getPlayerName();
+      Buzzes.insert({name: name});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    Meteor.methods({
+      removeAllBuzzes: function() {
+        return Buzzes.remove({});
+      }
+    });
+  });
+
+  Buzzes.allow({
+    insert: function() {
+      if (Buzzes.find().count() > 0)
+        return false;
+      else
+        return true;
+    }
+  });
+}
